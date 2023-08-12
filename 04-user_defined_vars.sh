eb70ada2fed7bdd700aa2d709104fdcdbc512dc4
David <bolarinwa29@gmail.com>
2023-08-12 16:24:30 -0500

    the script here are just for practising only

---
+++ 04-user_defined_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash      
+#title           :
+#description     :
+#author		     : David 
+#version         :1.0   
+
+name="Simon Legah"
+id=08112012
+
+echo "The name varibale value is: "$name
+echo "The id variable value is: "$id
+
+
---
+++ 08-arithmetic_operations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash      
+#title           : Arithmetic operations
+#author		     : David Bolarinwa 
+
+expr 3 + 2
+expr 3 - 2
+expr 3 \* 2
+expr 10 / 2
+expr 20 % 3
+echo addition of 3 and 2 is : `expr 3 + 2`
+
